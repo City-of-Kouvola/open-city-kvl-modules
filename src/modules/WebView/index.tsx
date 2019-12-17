9365bd379918dabffb2482c75879e347a68cc470
EetuKaunismaki <eetu.kaunismaki@punosmobile.com>
2019-12-17 16:30:12 +0200

    Fixed modal timeout, type to interface, removed comments

--- src/modules/WebView/index.tsx
+++ src/modules/WebView/index.tsx
@@ -1,38 +1,39 @@
 /* @flow */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 import PopupModal from 'modules/WebView/components/PopupModal';
 
-type Props = {
-  src: string; // Secure connection is required
-  modalVisible: boolean; // Is the warning modal shown
-  modalTimeout: number; // How long will the modal be shown before automatically closing
-  onLoadEnd: () => {};
-};
-
-type State = {
+interface Props {
+  src: string;
   modalVisible: boolean;
-};
+  modalTimeout: number;
+  onLoadEnd: () => {};
+}
 
 const WebViewModule = (props: Props) => {
   const [modalVisible, setModalVisible] = useState(true);
-  // Returns a component to be rendered on WebView error
+
   const onError = (): void => {
-    // TODO: Return a view containing the error
     console.log('WebView Error');
   };
 
-  const hidemodal = (): void => {
+  const hideModal = (): void => {
     setModalVisible(false);
   };
+
+  useEffect(() => {
+    setTimeout(() => {
+      hideModal();
+    }, props.modalTimeout);
+  }, []);
 
   return (
     <View style={{ flex: 1 }}>
       <PopupModal
         modalVisible={modalVisible}
         modalText={`Sinut on ohjattu verkko-osoitteeseen ${props.src}`}
-        onClose={hidemodal}
+        onClose={hideModal}
       />
       <WebView
         geolocationEnabled={true}
