b36af1b27b722b4212cd4a8ff83c594ebb56b77c
EetuKaunismaki <eetu.kaunismaki@punosmobile.com>
2019-12-18 10:03:33 +0200

    Removed some lines from index

--- index.tsx
+++ index.tsx
@@ -1,8 +1,2 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
 import WebViewModule from 'modules/WebView';
-
-AppRegistry.registerComponent(appName, () => App);
-
 export { WebViewModule };
